5a72e69fb7e6c992c0a0b0330774d33d6209a728
fazila7002 <fazilatunnisa705@gmail.com>
2026-04-01 17:21:05 +0700

    implementasi logika switchcase, prosedur output, dan fungsi main

--- switchCase.cpp
+++ switchCase.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 using namespace std;
 
-// Deklarasi variable global
+
 int kodeJurusan;
 string namaJurusan;
 string statusKuota;
 
-// prosedur input
+
 void input() {
     cout << "=== PENDAFTARAN JURUSAN KAMPUS ===" << endl;
     cout << "1. Teknologi Informasi" << endl;
@@ -15,4 +15,40 @@ void input() {
     cout << "----------------------------------" << endl;
     cout << "Masukkan kode jurusan (1-3): ";
     cin >> kodeJurusan;
+}
+
+void pilihanJurusan() {
+    switch (kodeJurusan) {
+        case 1:
+            namaJurusan = "Teknologi Informasi";
+            statusKuota = "Tersedia (15 Kursi)";
+            break;
+        case 2:
+            namaJurusan = "Teknik Elektro";
+            statusKuota = "Kuota Penuh!";
+            break;
+        case 3:
+            namaJurusan = "Sistem Informasi";
+            statusKuota = "Tersedia (5 Kursi)";
+            break;
+        default:
+            namaJurusan = "Tidak Diketahui";
+            statusKuota = "Error: Kode Tidak Valid";
+    }
+}
+
+
+void output() {
+    cout << "\n=== HASIL SELEKSI ===" << endl;
+    cout << "Jurusan Pilihan : " << namaJurusan << endl;
+    cout << "Status Kuota    : " << statusKuota << endl;
+    cout << "----------------------------------" << endl;
+}
+
+int main() {
+    input();
+    pilihanJurusan();
+    output();
+
+    return 0;
 }
