889afb7377d848bf379474c8982509fa118818ca
fazila7002 <fazilatunnisa705@gmail.com>
2026-04-01 17:07:30 +0700

    buat file logika_or.cpp dan setup input nilai

---
+++ logika_or.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    float nilM,nilB,rerata;
+    string status;
+
+    cout<<"Masukkan nilai Matematika = ";
+    cin>>nilM;
+    cout<<"Masukkan nilai Bahasa Inggris = ";
+    cin>>nilB;
+
+    rerata = (nilB+nilM)/2;
+}
