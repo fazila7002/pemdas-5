49f5bccccce13f2c5e6cceb762ada18167ed8082
fazila7002 <fazilatunnisa705@gmail.com>
2026-04-01 17:02:18 +0700

    update logika perulangan DO...WHILE

--- looping.cpp
+++ looping.cpp
@@ -24,4 +24,11 @@ cout<<"Bilangan acak while yang terakhir ="<<x <<endl;
 
 cout<<"PERULANGAN DO...WHILE"<<endl;
     x = 1 + rand()%10;
+do{
+        cout<<"Bilangan acak = "<<x <<endl;
+        x = rand()%10;
+    }while(x<=5);
+
+    cout<<"Bilangan acak do-while yang terakhir ="<<x <<endl;
+    cout<<endl;
 }
